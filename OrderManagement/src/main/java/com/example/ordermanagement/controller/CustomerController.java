e22a8371f18986bedae44fb5e460f3954f543e0d
iSeFz <seifyahia@gmail.com>
2023-12-28 23:23:51 +0200

    Updated CustomerController to use the CustomerService Added createAccount endpoint to add a new customer Added deductCost endpoint to decrement the balance of certain customer Added testing endpoint to return all system customers

--- OrderManagement/src/main/java/com/example/ordermanagement/controller/CustomerController.java
+++ OrderManagement/src/main/java/com/example/ordermanagement/controller/CustomerController.java
@@ -1,7 +1,46 @@
 package com.example.ordermanagement.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.example.ordermanagement.service.CustomerService;
+import com.example.ordermanagement.model.Customer;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.*;
+
+// Rest Controller for customers
 @RestController
+@RequestMapping("/customers")
 public class CustomerController {
+    // Customer Service to be injected
+    private final CustomerService customerService;
+
+    // Constructor to inject the service
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    // Create a new account for a customer
+    @PostMapping("/createAccount")
+    public Customer createAccount(@RequestParam String name, @RequestParam String email,
+            @RequestParam String password, @RequestParam double balance) {
+        return customerService.addCustomer(name, email, password, balance);
+    }
+
+    // Deduct the cost of an order from the customer's balance
+    @PutMapping("/deductCost/{cost}")
+    public String deductCost(@RequestBody Customer customer, @PathVariable("cost") double cost) {
+        return customerService.deductCost(customer, cost);
+    }
+
+    /* // Deduct the shipping fees from the customer's balance
+    @PutMapping("/deductShippingFees/{fees}")
+    public String deductShippingFees(@RequestBody Customer customer, @PathVariable("fees") double fees) {
+        return customerService.deductShippingFees(customer, fees);
+    } */
+
+    // Get all system customers
+    @GetMapping("/getAllCustomers")
+    public List<Customer> getAllCustomers() {
+        return customerService.getCustomers();
+    }
 }
