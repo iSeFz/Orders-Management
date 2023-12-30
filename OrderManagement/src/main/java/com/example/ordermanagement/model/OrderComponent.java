94ca88babf366c5ac4e57030dd37a1bb7f552ac9
iSeFz <seifyahia@gmail.com>
2023-12-30 11:06:11 +0200

    Modified OrderComponent model to be an abstract class Merged common attributes from simple & compound orders Added getters and setters for all attributes

--- OrderManagement/src/main/java/com/example/ordermanagement/model/OrderComponent.java
+++ OrderManagement/src/main/java/com/example/ordermanagement/model/OrderComponent.java
@@ -1,8 +1,53 @@
 package com.example.ordermanagement.model;
 
-public interface OrderComponent {
-    public String listDetails();
-    public String getLocation();
-    public double getShippingFees();
-    public Customer getCustomer();
+// Abstract class to represent an order
+public abstract class OrderComponent {
+    private String location;
+    private Customer customer;
+    private double totalCost;
+    private double shippingFees;
+    private boolean isShipped;
+
+    // Abstract method to list order details, acts as a getter
+    public abstract String listDetails();
+
+    public String getLocation() {
+        return this.location;
+    }
+
+    public Customer getCustomer() {
+        return this.customer;
+    }
+
+    public double getTotalCost() {
+        return totalCost;
+    }
+
+    public double getShippingFees() {
+        return this.shippingFees;
+    }
+
+    public boolean isShipped() {
+        return this.isShipped;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public void setShippingFees(double shippingFees) {
+        this.shippingFees = shippingFees;
+    }
+
+    public void setTotalCost(double totalCost) {
+        this.totalCost = totalCost;
+    }
+
+    public void setShipped(boolean isShipped) {
+        this.isShipped = isShipped;
+    }
 }
