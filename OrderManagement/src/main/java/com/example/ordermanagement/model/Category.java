82659e9d379cc6bce677d693e76e3dda13226de5
iSeFz <seifyahia@gmail.com>
2023-12-30 10:00:25 +0200

    Modified Category model Added default constructor & removeProduct method Added comments and formatted the document

--- OrderManagement/src/main/java/com/example/ordermanagement/model/Category.java
+++ OrderManagement/src/main/java/com/example/ordermanagement/model/Category.java
@@ -1,26 +1,50 @@
 package com.example.ordermanagement.model;
+
 import java.util.ArrayList;
 import java.util.List;
+
 public class Category {
     private String name;
     private List<Product> products;
-    private int remainingItemsForCategory;
+    private int remainingItems;
 
-    public Category(String name) {
-        this.name = name;
-        this.products = new ArrayList<>();
-        remainingItemsForCategory = 0;
+    // Default constructor
+    public Category() {
+        this.name = "";
+        this.products = new ArrayList<Product>();
+        this.remainingItems = this.products.size();
     }
 
+    // Parameterized constructor
+    public Category(String name) {
+        this();
+        this.name = name;
+    }
+
+    // Add a new product to the category
+    public void addProduct(Product product) {
+        this.products.add(product);
+        this.remainingItems += product.getRemainingCount();
+    }
+
+    // Remove a product from the category
+    public void removeProduct(Product product) {
+        this.products.remove(product);
+        this.remainingItems -= product.getRemainingCount();
+    }
+
+    // Return the name of the category
+    public String getName() {
+        return this.name;
+    }
+
+    // Return all products in the category
     public List<Product> getProducts() {
         return this.products;
     }
-    public void addProduct(Product p){
-        products.add(p);
-        remainingItemsForCategory+=p.getRemainingCount();
+
+    // Return the remaining amount of all products in the category
+    public int getRemainingItems() {
+        return this.remainingItems;
     }
-    public String getName(){
-        return name;
-    }
-    public int getRemainingItemsForCategory(){return remainingItemsForCategory;}
 }
